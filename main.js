63f729204947db5472dbceeb70efdd92e97ba401
Your Name <you@example.com>
2020-05-21 15:05:59 +0300

    Added styles

--- main.js
+++ main.js
@@ -22,7 +22,8 @@ function recalculate() {
         exponent_bits.push((array[i].checked+0).toString());
     }
     let exponent = parseInt(exponent_bits.join(''), 2) - 1023;
-    getById('exponent_bits').innerHTML = parseInt(exponent_bits.join(''), 2);
+    let enc_exponent = parseInt(exponent_bits.join(''), 2);
+    getById('exponent_bits').innerHTML = enc_exponent;
     getById('exponent').innerHTML = exponent;
 
 
@@ -33,10 +34,24 @@ function recalculate() {
         mantissa_bits.push((array[i].checked+0).toString());
     }
     let mantissa = 1 + parseInt(mantissa_bits.join(''), 2) / Math.pow(2, 52);
-    getById('mantissa_bits').innerHTML = parseInt(mantissa_bits.join(''), 2);
+    let enc_mantissa = parseInt(mantissa_bits.join(''), 2);
+    getById('mantissa_bits').innerHTML = enc_mantissa;
     getById('mantissa').innerHTML = parseFloat(mantissa);
 
-    getById('decimal').value = sign * Math.pow(2, exponent) * mantissa;
+    if (enc_exponent == 0) {
+        getById('mantissa').innerHTML += ' (denormalized)';
+        getById('exponent').innerHTML += ' (denormalized)';
+    }
+    let val = (enc_exponent + enc_mantissa);
+    if (val == 0) {
+        getById('decimal').value = (getById('sign').checked ? '-' : '')+'0.0';
+    } else if (val == 4503599627372542 ) {
+        getById('decimal').value = NaN;
+    // } else if (val == 1 ) {
+    //     getById('decimal').value = '-'+0+'.0';
+    } else {
+        getById('decimal').value = sign * Math.pow(2, exponent) * mantissa;
+    }
 }
 
 document.querySelectorAll('.bit').forEach(function(elem) {
