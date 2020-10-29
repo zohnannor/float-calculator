6f275fc6c788f4eac00b9288b45a68a30259f1b1
Zohnannor <herovblin@gmail.com>
2020-10-29 15:56:45 +0300

    Updates

---
+++ precache-manifest.54c1f842cd95c0a9494f451a3d496cf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6209b7d19ff7e2d5e6ff37c7764061",
+    "url": "/float-calculator/index.html"
+  },
+  {
+    "revision": "f0bcc3a10e6fddec04aa",
+    "url": "/float-calculator/static/css/main.5651e3f3.chunk.css"
+  },
+  {
+    "revision": "bfba02601a95099d74e6",
+    "url": "/float-calculator/static/js/2.00543f18.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/float-calculator/static/js/2.00543f18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0bcc3a10e6fddec04aa",
+    "url": "/float-calculator/static/js/main.0c06ed2b.chunk.js"
+  },
+  {
+    "revision": "a7db8a480275960148dc",
+    "url": "/float-calculator/static/js/runtime-main.022c2a2e.js"
+  }
+]);
